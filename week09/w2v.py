edef37545b0953cf996dd2c47a14cc49b2edc5cd
PeterWolf <peter.w@droidtown.co>
2025-11-05 23:38:57 +0800

    釋出實驗用程式

---
+++ week09/w2v.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import fasttext
+import fasttext.util
+
+def main():
+    """"""
+    return None
+
+
+if __name__ == "__main__":
+
+    ft = fasttext.load_model("cc.en.300.bin")
+    fasttext.util.reduce_model(ft, 10)
+    print(ft.get_dimension())
+
+    print(ft.get_nearest_neighbors('hello'))
